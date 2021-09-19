73c9ca172fe34d104be32e7bdd4ba473264897b6
sujen <sujenphea@gmail.com>
2021-09-19 18:26:16 +1200

    Initial Commit

---
+++ Models/Daily.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoodTracker.Models
+{
+    public class Daily
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Summary { get; set; } = null!;
+
+        [Required]
+        public int UserId { get; set; }
+
+        public User User { get; set; } = null!;
+
+        public string? DateCreated { get; set; }
+    }
+}
